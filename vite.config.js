b9aed6ccc79befe5325e139b64664e1b3ecfa0d2
arthurL3mos <arthurdasilvalemos1@gmail.com>
2025-04-24 23:28:02 -0300

    correcoes no deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/buscardor-perfil-github",
+  base: "/buscador-perfil-github",
   plugins: [react()],
 })
